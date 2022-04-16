b679678115c400e01f69540966b1709fcde4b551
Javier <javier.sardinas@gmail.com>
2022-04-16 00:12:06 -0500

    Add text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ currentDayElement = $('#currentDay');
 
 $(document).ready(()=>{
     var now = moment();
-    console.log(now);
+    
     currentDayElement.text(now.format("dddd, MMMM Do YYYY"));
     let tb = $('.time-blocks');
     for(let i = 0, hour = moment("00:00:00", "hh:mm:ss"); i < 24; ++i, hour.add(1,'hours')){
@@ -11,35 +11,36 @@ $(document).ready(()=>{
 
         let timeCol = $('<div>');
         timeCol.addClass('col-1');
-        timeCol.css('border-top', '1px solid #CCC');
-        timeCol.css('text-align', 'right');
-        timeCol.css('padding-top', '20px');
+        timeCol.addClass('hour');
+        // timeCol.css('border-top', '1px solid #CCC');
+        // timeCol.css('text-align', 'right');
+        // timeCol.css('padding-top', '20px');
         timeCol.text(hour.format('hA'));
 
         let eventCol = $('<div>');
         eventCol.addClass('col-10');
-        console.log(hour);
+        eventCol.css('display','flex');
+        let eventInput = $('<textarea>');
+        eventInput.css('width', '100%');
+        
+        eventCol.append(eventInput);
+        
         if (now < hour){
             timeCol.addClass('future-event-time')
-            eventCol.addClass('future-event');
+            eventCol.addClass('future');
         }
         else {
             timeCol.addClass('past-event-time')
+            eventInput.prop('disabled', true);
             if (now < hour.clone().add(1,'hours'))
-                eventCol.addClass('current-event');
+                eventCol.addClass('present');
             else
-                eventCol.addClass('past-event');
+                eventCol.addClass('past');
         }
         
         let lastCol = $('<div>');
         lastCol.addClass('col-1');
-        lastCol.css('background-color', '#0EB0CB');
-        lastCol.css('border-left', '4px solid #0A8498');
-        lastCol.css('border-radius', '0px 10px 10px 0px');
-        lastCol.css('display', 'flex');
-        lastCol.css('justify-content', 'center');
-        lastCol.css('align-items', 'center');
-        lastCol.css('color', 'white');
+        lastCol.addClass('saveBtn');
 
         let saveIcon = $('<i>');
         saveIcon.addClass('far fa-save');
